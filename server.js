00bdb46662eb9fa6db4023f34b5c4a37d22f1a2c
Brigham Mueller <brigmu@gmail.com>
2020-02-22 11:45:22 -0800

    added server

--- server.js
+++ server.js
@@ -1,78 +1,50 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var path = require("path");
+const express = require('express');
+const path = require('path');
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const port = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
-// =============================================================
-let tables = [];
-let waitlist = [];
-
-// Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/view", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
-});
-
-app.get("/reserve, function(req, res) {
+app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
   });
 
-
-// Displays all characters
-app.get("/api/current-tables", function(req, res) {
-  return res.json(tables);
+app.get("/api/tables", function(req, res) {
+    return res.json(savedCustomers.tables);
 });
 
-// Displays a single character, or returns false
-app.get("/api/wait-list", function(req, res) {
-  var chosen = req.params.character;
+app.get("/api/waitlist", function(req, res) {
+    return res.json(savedCustomers.waitList);
+});
 
-  console.log(chosen);
+app.post("/api/tables", function(req, res) {
+    let newReservation = req.body;
 
-  for (var i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
+    let currentTable = savedCustomers.tables.length;
+    if (savedCustomers.tables.length <= 5) {
+        savedCustomers.tables.push(newReservation);
+        res.json({response: 'Added to table'});
+    } else {
+        savedCustomers.waitList.push(newReservation);
+        res.json({response: 'Added to waitlist'});
     }
-  }
+})
 
-  return res.json(false);
-});
+app.listen(port, () =>{
+    console.log(`Server listening on port ${port}`);
+})
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newCharacter = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newCharacter);
-
-  characters.push(newCharacter);
-
-  res.json(newCharacter);
-});
-
-// Starts the server to begin listening
-// =============================================================
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
-});
+const savedCustomers = {
+    tables: [],
+    waitList: []
+};
